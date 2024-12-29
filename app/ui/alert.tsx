e853e60574b4494eee26e5bce5d32d7e5580ba8d
Bigjbird1 <jdarsinos1@gmail.com>
2024-12-29 12:52:57 -0500

    Add files via upload

---
+++ app/ui/alert.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const Alert = ({ children }) => (
+  <div className="alert">
+    {children}
+  </div>
+);
+
+export const AlertDescription = ({ children }) => (
+  <div className="alert-description">
+    {children}
+  </div>
+);
+
