508f43fcbe0b0776f59a9b805a92e2c2b44c16ab
Bigjbird1 <jdarsinos1@gmail.com>
2024-12-29 12:07:38 -0500

    Delete WeddingTransfer directory

--- WeddingTransfer/app/listing/[id]/page.tsx
+++
@@ -1,6 +0,0 @@
-import ListingDetail from '../../components/ListingDetail';
-
-export default function ListingDetailPage() {
-  return <ListingDetail />;
-}
-
--- WeddingTransfer/app/password-recovery/page.tsx
+++
@@ -1,6 +0,0 @@
-import PasswordRecovery from '../components/PasswordRecovery';
-
-export default function PasswordRecoveryPage() {
-  return <PasswordRecovery />;
-}
-
