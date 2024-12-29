af3e351363f6ac0a1a36cb114a3210d355a5f0a8
Bigjbird1 <jdarsinos1@gmail.com>
2024-12-29 12:03:25 -0500

    Update next.config.js

--- WeddingTransfer/next.config.js
+++ WeddingTransfer/next.config.js
@@ -1,34 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
-    domains: ['placeholder.com'], // Add any image domains you're using
-  },
-  experimental: {
-    appDir: true,
-  },
-  webpack: (config, { isServer }) => {
-    // Disable asm.js optimizations
-    config.optimization.minimizer.forEach((minimizer) => {
-      if (minimizer.constructor.name === 'TerserPlugin') {
-        minimizer.options.terserOptions = {
-          ...minimizer.options.terserOptions,
-          ecma: 5,
-          compress: {
-            ...minimizer.options.terserOptions.compress,
-            asmjs: false,
-          },
-          mangle: {
-            ...minimizer.options.terserOptions.mangle,
-            keep_fnames: true,
-          },
-        };
-      }
-    });
-
-    return config;
-  },
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  }
 }
 
 module.exports = nextConfig
