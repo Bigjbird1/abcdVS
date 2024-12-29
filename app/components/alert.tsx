b3124708e8d97944da7f2d56b6dfa6a663fcde64
Bigjbird1 <jdarsinos1@gmail.com>
2024-12-29 12:24:17 -0500

    Create alert.tsx

---
+++ app/components/alert.tsx
@@ -0,0 +1,14 @@
+// app/components/ui/alert.tsx
+import React from 'react';
+
+export const Alert = ({ children }) => (
+  <div className="alert">
+    {children}
+  </div>
+);
+
+export const AlertDescription = ({ children }) => (
+  <div className="alert-description">
+    {children}
+  </div>
+);
