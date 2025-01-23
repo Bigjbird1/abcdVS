b0c9c9c7345cca49fe260f0e4a08ca9863c92341
Jesse Darsinos <jessedarsinos@Jesses-Laptop.local>
2025-01-23 00:13:53 -0500

    Fix EnhancedSearch import path in search page

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,12 +1,12 @@
 'use client'
 
 import { useSearchParams } from 'next/navigation'
-import EnhancedSearch from '../components/EnhancedSearch'
+import EnhancedSearch from '../EnhancedSearch'
 
 export default function SearchPage() {
   const searchParams = useSearchParams()
-  const searchType = searchParams.get('type') || 'dates'
+  const searchType = searchParams?.get('type') ?? 'dates'
 
-  return <EnhancedSearch initialSearchType={searchType} />;
+  return <EnhancedSearch initialSearchType={searchType as 'dates' | 'marketplace'} />;
 }
 
